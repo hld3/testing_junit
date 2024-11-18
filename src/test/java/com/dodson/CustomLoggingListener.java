a02274d2774e38da2bdc185db1ee4c61e74810ac
Harry Dodson <harry.lee.dodson@gmail.com>
2024-11-18 12:11:16 -0500

    added META-INF/services file, now seeing custom logs

--- src/test/java/com/dodson/CustomLoggingListener.java
+++ src/test/java/com/dodson/CustomLoggingListener.java
@@ -8,18 +8,27 @@ public class CustomLoggingListener implements TestExecutionListener {
 
     @Override
     public void executionStarted(TestIdentifier testIdentifier) {
-        System.out.println("Test Started: " + testIdentifier.getDisplayName());
+        if (testIdentifier.isTest()) {
+            System.out.println("[CustomLoggingListener] Test Started: " + testIdentifier.getDisplayName());
+        }
     }
 
     @Override
     public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
         if (testIdentifier.isTest()) {
             String status = testExecutionResult.getStatus().toString();
-            System.out.println("Test Finished: " + testIdentifier.getDisplayName());
+            System.out.println("[CustomLoggingListener] Test Finished: " + testIdentifier.getDisplayName() + " - Status: " + status);
             testExecutionResult.getThrowable().ifPresent(throwable -> {
                 System.out.println("Test Failed with Exception:");
                 throwable.printStackTrace(System.out);
             });
+        }
+    }
+
+    @Override
+    public void executionSkipped(TestIdentifier testIdentifier, String reason) {
+        if (testIdentifier.isTest()) {
+            System.out.println("[CustomLoggingListener] Test Skipped: " + testIdentifier.getDisplayName());
         }
     }
 }
